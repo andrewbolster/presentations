fa91092868b2dc9e1e4efe48b682704d702139af
andrewbolster <andrewbolster@users.noreply.github.com>
2025-12-27 11:35:43 +0000

    deploy: 1fc9f4ba7c50c82d3c6ff057765e8c2842353816

--- themes/black-duck/setup.ts
+++
@@ -1,7 +0,0 @@
-import { defineAppSetup } from '@slidev/types'
-import Logo from './components/Logo.vue'
-
-export default defineAppSetup(({ app }) => {
-  // Register Logo component globally
-  app.component('Logo', Logo)
-})
