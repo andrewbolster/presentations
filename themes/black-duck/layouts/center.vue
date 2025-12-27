0420f9e75f39ab361525e9c7fa5963adac8ac99f
andrewbolster <andrewbolster@users.noreply.github.com>
2025-12-27 11:56:54 +0000

    deploy: 802e21df0de6f56791cd35d8d8eca43a8a264091

--- themes/black-duck/layouts/center.vue
+++ themes/black-duck/layouts/center.vue
@@ -3,6 +3,9 @@
     <div class="center-content">
       <slot />
     </div>
+    <div class="slide-footer">
+      Black Duck &copy; 2025
+    </div>
   </div>
 </template>
 
@@ -13,9 +16,21 @@
   justify-content: center;
   text-align: center;
   padding: 3rem 4rem;
+  position: relative;
+  height: 100%;
 }
 
 .center-content {
   max-width: 800px;
+}
+
+.slide-footer {
+  position: absolute;
+  bottom: 1rem;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 0.75rem;
+  color: var(--bd-text);
+  opacity: 0.5;
 }
 </style>
